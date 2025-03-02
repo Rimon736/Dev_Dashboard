e7b4e087c3abc1481df113ff2a215d965f3cc1ee
Rimon736 <haqueiftekharul84@gmail.com>
2025-03-02 21:03:09 +0600

    added blog page

--- scripts/event.js
+++ scripts/event.js
@@ -61,6 +61,28 @@ function getCurrDate(){
 
     return [today, currDate];
 }
+
 const [today, currDate] = getCurrDate();
 document.getElementById("curr-day").innerText = today;
 document.getElementById("curr-date").innerText = currDate;
+
+document.getElementById("blog-link").addEventListener("click", function () {
+    window.location.href = "blog.html";
+  });
+
+document.getElementById("home-link").addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("button clicked")
+    window.location.href = "index.html";
+
+})
+
+document.getElementById('theme-Button').addEventListener('click', function(event){
+    
+    const colors = ["#ffb6c1", "#e6e6fa", "#87cefa", "#98fb98", "#ffffe0", "#ffdab9", "#f5fffa", "#f0fff0", "#f08080", "#ffe4e1"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById('body').style.backgroundColor = randomColor;
+
+    console.log("selected color: ", randomColor);
+  
+})
