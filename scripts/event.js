0f21b370610963193b7a6e10525e0506fa99279a
Rimon736 <haqueiftekharul84@gmail.com>
2025-03-02 16:36:15 +0600

    added interectivness in the cards and acitivity log

--- scripts/event.js
+++ scripts/event.js
@@ -1,6 +1,7 @@
 var buttons = document.getElementsByClassName("complete-btn");
 var taskCount = 0;
-var taskAssigned=5;
+var taskAssigned = 6;
+
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
@@ -8,11 +9,40 @@ for (var i = 0; i < buttons.length; i++) {
         this.disabled = true;
         this.style.backgroundColor = "gray";
         taskAssigned--;
+
+        if(taskAssigned == 0){
+            alert("Board Updated Successfully"+"\nTask Completed: " + taskCount + "\nTask Remaining: " + taskAssigned);
+            alert("Congratulations! You have completed all the tasks");
+        }
+        else{
+            alert("Board Updated Successfully"+"\nTask Completed: " + taskCount + "\nTask Remaining: " + taskAssigned);
+        }
+
         var taskCompleted = document.getElementById("completed-task");
         var convertedCompleteTask = parseInt(taskCompleted.innerText);
-        convertedCompleteTask++; // Increment the value
-        taskCompleted.innerText = convertedCompleteTask; // Update the UI
-        console.log(convertedCompleteTask);
-        console.log(taskCount);
+        convertedCompleteTask++;
+        taskCompleted.innerText = convertedCompleteTask;
+
+        var taskRemaining = document.getElementById("remaining-task");
+        var convertedRemainingTask= parseInt(taskRemaining.innerText);
+        convertedRemainingTask--;
+        taskRemaining.innerText = convertedRemainingTask;
+
+        var cardTitle= this.closest(".col-span-1").getElementsByClassName("card-title")[0].innerText;
+        var currTime= new Date().toLocaleString();
+        var message= `you have completed the task "${cardTitle}" on ${currTime}.`;
+        console.log(message);
+
+        var log= document.createElement("p");
+        log.innerText= message;
+        
+        var activityLog = document.getElementById("activity-log");
+        activityLog.appendChild(log);
+
+        var clearHistoryButton = document.getElementById("clear-history");
+        clearHistoryButton.addEventListener("click", function() {
+            var activityLog = document.getElementById("activity-log");
+            activityLog.innerHTML = ""; 
+        });
     });
 }
